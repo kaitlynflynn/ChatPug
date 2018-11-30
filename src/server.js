064fb3bac0c6041c3b253f74d5b4bca74cfeb94f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-29 18:21:42 -0800

    Defined token route to generate token

--- src/server.js
+++ src/server.js
@@ -1,6 +1,36 @@
+let twilio = require("twilio");
 let express = require("express");
 
 let app = express();
+
+const TWILIO_ACCOUNT_SID = "TWILIO_ACCOUNT_SID";
+const TWILIO_CHAT_SERVICE_SID = "TWILIO_CHAT_SERVICE_SID";
+const TWILIO_API_KEY = "TWILIO_API_KEY";
+const TWILIO_API_SECRET = "TWILIO_API_SECRET";
+
+
+app.get("/token", function(req, res) {
+  let username = req.query.username;
+
+  let token = new AccessToken(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_API_KEY,
+      process.env.TWILIO_API_SECRET,
+      {
+          identity: username
+      }
+  );
+
+
+let grant = new ChatGrant({ serviceSid: process.env.TWILIO_CHAT_SERVICE_SID });
+
+
+  token.addGrant(grant);
+  const tokenJwt = token.toJwt();
+  console.log("token: " + tokenJwt);
+
+  res.send(tokenJwt);
+});
 
 app.get("/", function(req, res) {
   res.sendFile("/public/index.html", { root: __dirname });
